636ee5eef762c0c5332e90c96b1b647f95b9a284
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:50:49 +0700

    Inisialisasi dan Assignment Nilai, 28 Desember 2021

---
+++ main.c
@@ -0,0 +1,16 @@
+/* Penulis : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com*/
+/* Mengisi variable i yang bertype in dengan nilai 5 menuliskan */
+
+#include <stdio.h>
+int main(){
+    /* KAMUS */
+    float f = 0.555;
+    float x = 1.5E3;
+    
+    /* ALGORITMA */
+    printf ("Ini nilai f : %f \n", f);
+    printf ("Ini nilai f : %5.2f \n", f);
+    printf ("Ini nilai x : %10.2f \n", x);
+    
+    return 0;
+}
